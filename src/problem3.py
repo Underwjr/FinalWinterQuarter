9518970cf6b96df6d30312476eb9d117b1d2fffb
brackinPatsy <brackin@rose-hulman.edu>
2019-02-20 10:57:21 -0500

    initial version

--- src/problem3.py
+++ src/problem3.py
@@ -1,7 +1,7 @@
 """
 Exam 1, problem 3.
 
-Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
+Authors: Patsy_Brackin but mostly David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
          and PUT_YOUR_NAME_HERE.
 """  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
